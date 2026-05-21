1293ce2fd2928c784720b2c03c5e9bd06fb783f5
louisroquiny <88196785+louisroquiny@users.noreply.github.com>
2026-05-21 14:42:46 +0200

    Load no talking rules patch

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './defaultMissionsPatch.js'
 import './timerInputPatch.js'
+import './rulesSpeechPatch.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
