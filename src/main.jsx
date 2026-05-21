ea48b793d3cb5bd1eddcc9b34eac1a92399fe1d9
louisroquiny <88196785+louisroquiny@users.noreply.github.com>
2026-05-21 15:09:01 +0200

    Load default timer patch

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import './defaultMissionsPatch.js'
 import './timerInputPatch.js'
 import './rulesSpeechPatch.js'
+import './defaultTimerPatch.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
