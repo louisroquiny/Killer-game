b30e7b931a04de07091ede0759d6ba9671d5532b
louisroquiny <88196785+louisroquiny@users.noreply.github.com>
2026-05-28 13:37:20 +0200

    Load timeout victory patch

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './defaultMissionsPatch.js'
 import './timerInputPatch.js'
 import './rulesSpeechPatch.js'
 import './defaultTimerPatch.js'
+import './timeoutVictoryPatch.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
